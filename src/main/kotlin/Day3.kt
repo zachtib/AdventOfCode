2f591ba9f3126e5e1521112a1c446e21a851b9c2
Zach Tibbitts <ztibbitts@etsy.com>
2021-12-03 08:57:50 -0500

    stubs for Day 3

---
+++ src/main/kotlin/Day3.kt
@@ -0,0 +1,17 @@
+import util.part1Result
+import util.part2Result
+import util.resource
+
+fun day3Part1(input: Any): Int {
+    return -1
+}
+
+fun day3Part2(input: Any): Int {
+    return -1
+}
+
+fun main() {
+    val input = resource("day3.txt").asStrings()
+    day3Part1(input).part1Result()
+    day3Part2(input).part2Result()
+}
---
+++ src/test/kotlin/Day3Tests.kt
@@ -0,0 +1,20 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day3Tests {
+    private val input = """
+        
+    """.asTestData()
+
+    @Test
+    fun testPart1Givens() {
+        val actual = day3Part1(input)
+        assertEquals(0, actual)
+    }
+
+    @Test
+    fun testPart2Givens() {
+        val actual = day3Part2(input)
+        assertEquals(0, actual)
+    }
+}
