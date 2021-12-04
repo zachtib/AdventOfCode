78aae7d9e89829b9fea4f69fa1cad24d82363684
Zach Tibbitts <zachtib@gmail.com>
2021-12-04 10:34:37 -0500

    Initial Day4 setup

---
+++ src/main/kotlin/Day4.kt
@@ -0,0 +1,18 @@
+import util.asInts
+import util.part1Result
+import util.part2Result
+import util.resource
+
+fun day4Part1(input: Any): Int {
+    return -1
+}
+
+fun day4Part2(input: Any): Int {
+    return -1
+}
+
+fun main() {
+    val input = resource("day4.txt").asInts()
+    day4Part1(input).part1Result()
+    day4Part2(input).part2Result()
+}
