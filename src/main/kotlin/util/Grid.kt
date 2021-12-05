c19b9bc20702c08e1d12e3602ed560524cf84e57
Zach Tibbitts <zachtib@gmail.com>
2021-12-05 11:20:24 -0500

    check in for now

--- src/main/kotlin/util/Grid.kt
+++ src/main/kotlin/util/Grid.kt
@@ -1,38 +1,47 @@
 package util
 
-class Grid<T> private constructor(
-    val rowCount: Int,
-    val columnCount: Int,
-    private val data: MutableList<T>,
-) {
-//) : Map<Pair<Int, Int>, T> {
-
-    companion object {
-        fun <T> build(rows: Int, columns: Int, builder: (row: Int, column: Int) -> T): Grid<T> {
-            val data = mutableListOf<T>()
-            for (row in 0 until rows) {
-                for (column in 0 until columns) {
-                    data.add(builder(row, column))
-                }
-            }
-            return Grid(rows, columns, data)
-        }
-    }
-
+interface Grid<T> {
+    val rowCount: Int
+    val columnCount: Int
     val rows: List<List<T>>
+    val columns: List<List<T>>
+
+    operator fun get(row: Int, column: Int): T
+}
+
+interface MutableGrid<T> : Grid<T> {
+    operator fun set(row: Int, column: Int, value: T)
+}
+
+class ListGrid<T>(
+    override val rowCount: Int,
+    override val columnCount: Int,
+    private val data: MutableList<T>,
+) : Grid<T> {
+
+    override val rows: List<List<T>>
         get() = data.windowed(columnCount)
 
-    val columns: List<List<T>>
+    override val columns: List<List<T>>
         get() = (0 until columnCount).map { index -> rows.map { it[index] } }
 
     val items: List<T>
         get() = data.toList()
 
-    operator fun get(key: Pair<Int, Int>): T {
-        val (row, column) = key
+    override operator fun get(row: Int, column: Int): T {
         if (row !in 0 until rowCount || column !in 0 until columnCount) {
             throw ArrayIndexOutOfBoundsException()
         }
         return data[row * columnCount + column]
     }
+}
+
+fun <T> gridBuilder(rows: Int, columns: Int, builder: (row: Int, column: Int) -> T): Grid<T> {
+    val data = mutableListOf<T>()
+    for (row in 0 until rows) {
+        for (column in 0 until columns) {
+            data.add(builder(row, column))
+        }
+    }
+    return ListGrid(rows, columns, data)
 }
