c23dde1d0e03734dc483c881e60929decd4467c5
Zach Tibbitts <ztibbitts@etsy.com>
2021-12-06 10:43:07 -0500

    Add 2020 tests

---
+++ src/test/kotlin/twentytwenty/Day16Tests.kt
@@ -0,0 +1,76 @@
+package twentytwenty
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class Day16Tests {
+    private val sample = """
+        class: 1-3 or 5-7
+        row: 6-11 or 33-44
+        seat: 13-40 or 45-50
+
+        your ticket:
+        7,1,14
+
+        nearby tickets:
+        7,3,47
+        40,4,50
+        55,2,20
+        38,6,12
+    """.trimIndent()
+
+    @Test
+    fun `test parsing`() {
+        val actual = parseInput(sample)
+        val expected = ParsedInput(
+                rules = listOf(
+                        Rule("class", 1..3, 5..7),
+                        Rule("row", 6..11, 33..44),
+                        Rule("seat", 13..40, 45..50),
+                ),
+                yourTicket = listOf(7, 1, 14),
+                otherTickets = listOf(
+                        listOf(7, 3, 47),
+                        listOf(40, 4, 50),
+                        listOf(55, 2, 20),
+                        listOf(38, 6, 12),
+                ),
+        )
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test scanning error rate`() {
+        val sampleInput = parseInput(sample)
+        val actual = calculateScanningErrorRate(sampleInput.rules, sampleInput.otherTickets)
+        assertEquals(71, actual)
+    }
+
+    private val pt2Sample = """
+        class: 0-1 or 4-19
+        row: 0-5 or 8-19
+        seat: 0-13 or 16-19
+
+        your ticket:
+        11,12,13
+
+        nearby tickets:
+        3,9,18
+        15,1,5
+        5,14,9
+    """.trimIndent()
+
+    @Test
+    fun `test solving`() {
+        val sampleInput = parseInput(pt2Sample)
+        val actual = solveMyTicket(sampleInput.rules, sampleInput.yourTicket, sampleInput.otherTickets.filter {
+            isTicketValid(it, sampleInput.rules)
+        })
+        val expected = mapOf(
+                "class" to 12,
+                "row" to 11,
+                "seat" to 13,
+        )
+        assertEquals(expected, actual)
+    }
+}
---
+++ src/test/kotlin/twentytwenty/Day1Tests.kt
@@ -0,0 +1,68 @@
+package twentytwenty
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class Day1Tests {
+    @Test
+    fun `test list sum pairs`() {
+        val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        val actual = numbers.getPairsSummingTo(10)
+        val expected = listOf(
+                1 to 9,
+                2 to 8,
+                3 to 7,
+                4 to 6,
+        )
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test list sum triples`() {
+        val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        val actual = numbers.getTriplesSummingTo(10)
+        val expected = listOf(
+                Triple(1, 2, 7),
+                Triple(1, 3, 6),
+                Triple(1, 4, 5),
+                Triple(2, 3, 5),
+        )
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test triple product function`() {
+        val triple = Triple(2, 3, 4)
+        val expected = 24
+        val actual = triple.product()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test part1 given`() {
+        val given = listOf(1721, 979, 366, 299, 675, 1456)
+
+        val pairs = given.getPairsSummingTo(2020)
+        assertEquals(1, pairs.size)
+        val pair = pairs.first()
+        assertEquals(1721 to 299, pair)
+
+        val expected = 514579
+        val actual = getPairsProduct(given)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test part2 given`() {
+        val given = listOf(1721, 979, 366, 299, 675, 1456)
+
+        val triples = given.getTriplesSummingTo(2020)
+        assertEquals(1, triples.size)
+        val triple = triples.first()
+        assertEquals(Triple(979, 366, 675), triple)
+
+        val expected = 241861950
+        val actual = getTriplesProduct(given)
+        assertEquals(expected, actual)
+    }
+}
