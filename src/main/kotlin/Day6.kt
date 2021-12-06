41604d7faac304dfcac2948e7767261e286bb745
Zach Tibbitts <zachtib@gmail.com>
2021-12-06 08:24:59 -0500

    Day 6 Setup

---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,13 @@
+import util.part1Result
+import util.resource
+
+fun day6Part1() = -1
+
+fun day6Part2() = -1
+
+fun main() {
+    val input = resource("day6.txt")
+
+    day6Part1().part1Result()
+    day6Part2().part1Result()
+}
---
+++ src/test/kotlin/Day6Tests.kt
@@ -0,0 +1,24 @@
+import util.resource
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day6Tests {
+
+    private val sampleInput = resource {
+        """
+            
+        """.trimIndent()
+    }
+
+    @Test
+    fun testPart1Givens() {
+        val actual = day6Part1()
+        assertEquals(0, actual)
+    }
+
+    @Test
+    fun testPart2Givens() {
+        val actual = day6Part2()
+        assertEquals(0, actual)
+    }
+}
