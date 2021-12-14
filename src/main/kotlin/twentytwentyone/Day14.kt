ac803501eff9add965aab3b6983d28dae77d73cd
Zach Tibbitts <ztibbitts@etsy.com>
2021-12-14 10:37:46 -0500

    Day 14 Part 1

--- src/main/kotlin/twentytwentyone/Day14.kt
+++ src/main/kotlin/twentytwentyone/Day14.kt
@@ -1,10 +1,69 @@
 package twentytwentyone
 
+import libadvent.util.toPair
+import res.Resource
+import res.asComplexType
+import res.load
+import util.EmptyCollectionException
 import util.part1
 import util.part2
 
-fun day14Part1(): Int {
-    throw NotImplementedError()
+
+@JvmInline
+value class PolymerTemplate(val template: String)
+
+data class PairInsertion(val pair: Pair<Char, Char>, val insertion: Char) {
+    companion object {
+        fun fromString(inputString: String): PairInsertion {
+            val (inputs, output) = inputString.split(" -> ", limit = 2)
+            return PairInsertion(
+                pair = inputs.toList().toPair(),
+                insertion = output.first()
+            )
+        }
+    }
+}
+
+fun Resource.asPolymerInputs() = asComplexType {
+    takeOne {
+        PolymerTemplate(it)
+    } to takeLines {
+        PairInsertion.fromString(it)
+    }
+}
+
+fun PolymerTemplate.applyPairInsertions(insertions: List<PairInsertion>): PolymerTemplate {
+    val elements = template.toMutableList()
+    var indexOffset = 1
+
+    for ((originalPairIndex, window) in elements.windowed(2).withIndex()) {
+        val pair = window.toPair()
+
+        for (rule in insertions.filter { it.pair == pair }) {
+            val newIndex = originalPairIndex + indexOffset
+            elements.add(newIndex, rule.insertion)
+            indexOffset++
+        }
+    }
+    return PolymerTemplate(elements.joinToString(""))
+}
+
+fun PolymerTemplate.getElementCounts(): Map<Char, Int> {
+    val uniqueElements = this.template.toSet()
+    return uniqueElements.associateWith { element ->  template.count { it == element  } }
+}
+
+fun day14Part1(template: PolymerTemplate, insertions: List<PairInsertion>): Int {
+    var polymer = template
+    repeat(10) {
+        polymer = polymer.applyPairInsertions(insertions)
+    }
+
+    val elementCounts = polymer.getElementCounts()
+    val minimum = elementCounts.minOfOrNull { entry -> entry.value } ?: throw EmptyCollectionException()
+    val maximum = elementCounts.maxOfOrNull { entry -> entry.value } ?: throw EmptyCollectionException()
+
+    return maximum - minimum
 }
 
 fun day14Part2(): Int {
@@ -12,6 +71,7 @@ fun day14Part2(): Int {
 }
 
 fun main() {
-    part1 { day14Part1() }
+    val (template, insertions) = load("2021/day14.txt").asPolymerInputs()
+    part1 { day14Part1(template, insertions) }
     part2 { day14Part2() }
 }
--- src/test/kotlin/twentytwentyone/Day14Tests.kt
+++ src/test/kotlin/twentytwentyone/Day14Tests.kt
@@ -25,12 +25,85 @@ class Day14Tests {
         CC -> N
         CN -> C
     """.trimIndent()
-    )
+    ).asPolymerInputs()
+
+    private val sampleTemplate = sampleInput.first
+    private val samplePairInsertions = sampleInput.second
+
+    @Test
+    fun `test single insertion`() {
+        val template = PolymerTemplate("AC")
+        val insertions = listOf(PairInsertion('A' to 'C', 'B'))
+
+        val actual = template.applyPairInsertions(insertions)
+        assertEquals("ABC", actual.template)
+    }
+
+    @Test
+    fun `test larger single insertion`() {
+        val actual = sampleTemplate.applyPairInsertions(samplePairInsertions)
+        assertEquals("NCNBCHB", actual.template)
+    }
+
+    @Test
+    fun `test that after step 5, it has length 97`() {
+        var polymer = sampleTemplate
+        repeat(5) {
+            polymer = polymer.applyPairInsertions(samplePairInsertions)
+        }
+        assertEquals(97, polymer.template.length)
+    }
+
+    @Test
+    fun `test that after step 10, it has length 3073`() {
+        var polymer = sampleTemplate
+        repeat(10) {
+            polymer = polymer.applyPairInsertions(samplePairInsertions)
+        }
+        assertEquals(3073, polymer.template.length)
+    }
+
+    @Test
+    fun `test that after step 10, B occurs 1749 times`() {
+        var polymer = sampleTemplate
+        repeat(10) {
+            polymer = polymer.applyPairInsertions(samplePairInsertions)
+        }
+        assertEquals(1749, polymer.template.count { it == 'B' })
+    }
+
+    @Test
+    fun `test that after step 10, C occurs 298 times`() {
+
+        var polymer = sampleTemplate
+        repeat(10) {
+            polymer = polymer.applyPairInsertions(samplePairInsertions)
+        }
+        assertEquals(298, polymer.template.count { it == 'C' })
+    }
+
+    @Test
+    fun `test that after step 10, H occurs 161 times`() {
+        var polymer = sampleTemplate
+        repeat(10) {
+            polymer = polymer.applyPairInsertions(samplePairInsertions)
+        }
+        assertEquals(161, polymer.template.count { it == 'H' })
+    }
+
+    @Test
+    fun `test that after step 10, N occurs 865 times`() {
+        var polymer = sampleTemplate
+        repeat(10) {
+            polymer = polymer.applyPairInsertions(samplePairInsertions)
+        }
+        assertEquals(865, polymer.template.count { it == 'N' })
+    }
 
     @Test
     fun `test part1 sample`() {
-        val actual = day14Part1()
-        assertEquals(0, actual)
+        val actual = day14Part1(sampleTemplate, samplePairInsertions)
+        assertEquals(1588, actual)
     }
 
     @Test
