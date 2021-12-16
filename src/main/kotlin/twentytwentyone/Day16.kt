1d11aeb63cc03dba4a21217d00fef2e64fcd4026
Zach Tibbitts <ztibbitts@etsy.com>
2021-12-16 08:43:38 -0500

    Day 16 Stubs

---
+++ src/main/kotlin/twentytwentyone/Day16.kt
@@ -0,0 +1,21 @@
+package twentytwentyone
+
+import libadvent.part1
+import libadvent.part2
+import libadvent.resource.load
+
+
+fun day16Part1(): Int {
+    throw NotImplementedError()
+}
+
+fun day16Part2(): Int {
+    throw NotImplementedError()
+}
+
+fun main() {
+    val input = load("2021/day16.txt")
+
+    part1 { day16Part1() }
+    part2 { day16Part2() }
+}
---
+++ src/test/kotlin/twentytwentyone/Day16Tests.kt
@@ -0,0 +1,26 @@
+package twentytwentyone
+
+import libadvent.resource.resourceOf
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day16Tests {
+
+    private val sampleInput = resourceOf(
+        """
+            
+        """.trimIndent()
+    )
+
+    @Test
+    fun `test part 1 example`() {
+        val actual = day16Part1()
+        assertEquals(0, actual)
+    }
+
+    @Test
+    fun `test part 2 example`() {
+        val actual = day16Part2()
+        assertEquals(0, actual)
+    }
+}
